21e1112a797136cede14f0165e1f41c0208e058c
jedit-bit <tabemohsen5@gmail.com>
2025-12-13 07:33:10 -0800

    Update dataset.py

--- eeg_clustering/dataset.py
+++ eeg_clustering/dataset.py
@@ -1,17 +1,17 @@
 import torch
-from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
 
-def get_cifar10_loaders(batch_size=64, resize=16):
+def get_dataloaders(batch_size=64):
     transform = transforms.Compose([
+        transforms.Resize((16, 16)),
         transforms.ToTensor(),
-        transforms.Resize((resize, resize)),
     ])
-    
+
     train_dataset = datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
     test_dataset = datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
-    
+
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
     test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
-    
+
     return train_loader, test_loader
